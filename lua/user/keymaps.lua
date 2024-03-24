41499689b235f80c671dc2adaf1cd1b5967c8979
nasser buhamad <arabgb@gmail.com>
2024-03-25 02:50:36 +0300

    Adding custom keymap for toggle tree

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -33,3 +33,11 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left wind
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- setup for nvim-trer
+vim.keymap.set('n', '<C-j>tt',':NvimTreeToggle<CR>')
+
+-- custom key
+-- exit from nvim
+vim.keymap.set('n', '<C-j>e',':exit<CR>')
+
