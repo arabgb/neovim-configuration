76bb482c05ae51fdfea4f8d1b2e585d2208153eb
nasser buhamad <arabgb@gmail.com>
2024-03-28 12:30:29 +0300

    Remove ToggleTerm, I prefer to use tmux insted of it

--- lua/user/plugins/toggleterm.lua
+++
@@ -1,6 +0,0 @@
-return
-{
-  -- amongst your other plugins
-  {'akinsho/toggleterm.nvim', version = "*", config = true}
-  -- {'akinsho/toggleterm.nvim', version = "*", opts = {--[[ things you want to change go here]]}}
-}
