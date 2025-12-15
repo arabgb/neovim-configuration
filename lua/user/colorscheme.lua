ce6ea2c08b016ebdc3aff34cb3e5d44f41848a1e
nasser buhamad <arabgb@gmail.com>
2025-12-15 10:54:04 +0300

    fix folder colors in nvimtree

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,2 +1,6 @@
 -- vim.cmd("set background=dark")
 vim.cmd("colorscheme ayu")
+-- color for collsaped folder
+vim.cmd("highlight NvimTreeFolderName guifg=#D8D8D8 ctermfg=LightGray")
+-- color for expanded folder
+vim.cmd("highlight NvimTreeOpenedFolderName guifg=#D8D8D8 ctermfg=LightGray")
