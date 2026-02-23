11a7f1fb514b49ac295f07f52aa926b67f78cca6
nasser buhamad <arabgb@gmail.com>
2026-02-23 12:44:07 +0300

    Learning lua with vim

--- init.lua
+++ init.lua
@@ -20,3 +20,22 @@ require("user.js-doc")
 require("user.nvim-cmp")
 require("user.workspace-diagnostics")
 -- TODO: I LOVE NVIM
+
+local function clean_my_code()
+	local current_view = vim.fn.winsaveview()
+	pcall(function()
+		vim.cmd([[ %s/\s\+$//e ]])
+	end)
+	vim.cmd([[ normal! gg=G ]])
+	vim.fn.winrestview(current_view)
+	print("Code Cleaned")
+end
+
+vim.keymap.set("n", "<leader>q", clean_my_code, { desc = " Clean My Code" })
+
+local function customize_ui()
+	vim.api.nvim_set_hl(0, "LineNr", { fg = "#00ff00", bold = false })
+	vim.api.nvim_set_hl(0, "CursorLine", { bg = "#2d3343" })
+end
+
+-- customize_ui()
