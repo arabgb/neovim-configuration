c9a6369fdb1b1e9913062a9dd9c041bdb1002c08
nasser buhamad <arabgb@gmail.com>
2026-05-22 00:03:52 +0300

    change theme to dark mode

--- lua/user/plugins/colorschema.lua
+++ lua/user/plugins/colorschema.lua
@@ -1,15 +1,15 @@
 return {
-	{
-		"Shatur/neovim-ayu",
-		name = "ayu",
-		priority = 1000, -- Load this before all other plugins
-		config = function()
-			-- Configure ayu settings if needed
-			require("ayu").setup({
-				mirage = true, -- Set to true to use the "Mirage" variant
-			})
-			-- Set the colorscheme
-			vim.cmd([[colorscheme ayu]])
-		end,
-	},
+    {
+        "Shatur/neovim-ayu",
+        name = "ayu",
+        priority = 1000, -- Load this before all other plugins
+        config = function()
+            -- Configure ayu settings if needed
+            require("ayu").setup({
+                mirage = false, -- Set to true to use the "Mirage" variant
+            })
+            -- Set the colorscheme
+            vim.cmd([[colorscheme ayu]])
+        end,
+    },
 }
